22f589257c26ccc1c2ada134e50e268f477b5a98
Lucas Hermann Negri <lucashnegri@gmail.com>
2023-06-12 13:26:34 -0400

    Preparação do projeto

--- score.js
+++ score.js
@@ -5,17 +5,13 @@ const router = express.Router();
 
 // Registrar um novo resultado
 router.post("/", async (req, res) => {
-  let collection = await db.collection("score");
-  let result = await collection.insertOne(req.body);
-  res.send(result).status(204);
+    // seu código aqui
 });
 
 
 // Pegar os 10 melhores resultados
 router.get("/", async (req, res) => {
-  let collection = await db.collection("score");
-  let resultado = await collection.find().sort({ pontos: -1 }).limit(10).toArray();
-  res.send(resultado).status(200);
+    // seu código aqui
 });
 
 module.exports = router;
